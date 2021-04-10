1ca82b6de091c3e3d0d75092663b661917fefc32
hanajimajo <hanakun0318@icloud.com>
2021-04-10 07:42:00 +0000

    Finish Commit5

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+
   root 'homes#top'
   get 'home/about' => 'homes#about'
-
-  devise_for :users
 
   resources :users,only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
   	get 'followings' => 'relationships#followings', as: 'followings'
   	get 'followers' => 'relationships#followers', as: 'followers'
   end
+
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
+
+  get '/search', to: 'search#search'
 
 end
